3f14e30dbd0623af4d7734a40561a1d887001fc8
jayanth909-wq <jackiejayanth909@gmail.com>
2025-09-14 11:47:05 +0530

    Table 9 Program

---
+++ src/Java_Programs_001.java
@@ -0,0 +1,15 @@
+public class Java_Programs_001 {
+    public static void main(String[] args) {
+        int table = 9;
+        System.out.printf("%dx 1 = %d", table*1, table);
+        System.out.printf("%dx 2 = %d", table*2, table);
+        System.out.printf("%dx 3 = %d", table*3, table);
+        System.out.printf("%dx 4 = %d", table*4, table);
+        System.out.printf("%dx 5 = %d", table*5, table);
+        System.out.printf("%dx 6 = %d", table*6, table);
+        System.out.printf("%dx 7 = %d", table*7, table);
+        System.out.printf("%dx 8 = %d", table*8, table);
+        System.out.printf("%dx 9 = %d", table*9, table);
+        System.out.printf("%dx 10 = %d", table*10, table);
+    }
+}
